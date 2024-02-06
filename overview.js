5d25fb73c90033d4230ad0702a8105c4f28a759d
1RqMi-Offical <155063799+1RqMi-Offical@users.noreply.github.com>
2024-02-06 20:31:09 +0300

    Update overview.js

--- overview.js
+++ overview.js
@@ -4,17 +4,17 @@ let back = document.querySelector(".container .back")
 overviewBtn.addEventListener("click", e => {
 
     console.log(location.pathname)
-    if (location.pathname == "/index.html") {
+    if (location.pathname == "/checkments.github.io/index.html") {
 
         console.log("GO overview")
-        location.href = "/overview.html"
+        location.href = "/checkments.github.io/overview.html"
     } else {
         console.log("GO check")
-        location.href = "/index.html"
+        location.href = "/checkments.github.io/index.html"
     }
 
 })
 
 back.addEventListener("click", e => {
-    location.href = "/index.html"
+    location.href = "/checkments.github.io/index.html"
 })
